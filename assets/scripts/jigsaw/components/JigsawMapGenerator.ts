9ae1aeac987e23799e9c8b3bfad4f1b8e8ec873d
thangvpd <thangvpd@vng.com.vn>
2024-03-12 16:21:58 +0700

    feat: add model for wave function collapse

---
+++ assets/scripts/jigsaw/components/JigsawMapGenerator.ts
@@ -0,0 +1,8 @@
+import { Component, _decorator } from "cc";
+
+const { ccclass, property } = _decorator;
+
+@ccclass('JigsawMapGenerator')
+export default class JigsawMapGenerator extends Component { 
+  
+}
---
+++ assets/scripts/jigsaw/models/Tile.ts
@@ -0,0 +1,59 @@
+const TOP = 0;
+const RIGHT = 1;
+const BOTTOM = 2;
+const LEFT = 3;
+export default class Tile {
+  public type: string = "";
+  public edges: number[] = [];
+  public up: number[] = [];
+  public right: number[] = [];
+  public down: number[] = [];
+  public left: number[] = [];
+
+  constructor(type: string, edges: number[]) {
+    this.type = type;
+    this.edges = edges;
+    this.up = [];
+    this.right = [];
+    this.down = [];
+    this.left = [];
+  }
+
+  compareEdge(edge1, edge2) {
+    return (edge1 === 1 && edge2 === 2) || (edge1 === 2 && edge2 === 1);
+  }
+
+  analyze(tiles: Tile[]): void {
+    for (let i = 0; i < tiles.length; i++) {
+      let tile = tiles[i];
+
+      // UP
+      if (this.edges[TOP] === 0) {
+        this.up = [];
+      } else {
+        this.compareEdge(tile[BOTTOM], this.edges[TOP]) && this.up.push(i);
+      }
+
+      // RIGHT
+      if (this.edges[RIGHT] === 0) {
+        this.right = [];
+      } else {
+        this.compareEdge(tile[LEFT], this.edges[RIGHT]) && this.right.push(i);
+      }
+
+      // BOTTOM
+      if (this.edges[BOTTOM] === 0) {
+        this.down = [];
+      } else {
+        this.compareEdge(tile[TOP], this.edges[BOTTOM]) && this.down.push(i);
+      }
+
+      // LEFT
+      if (this.edges[LEFT] === 0) {
+        this.right = [];
+      } else {
+        this.compareEdge(tile[RIGHT], this.edges[LEFT]) && this.left.push(i);
+      }
+    }
+  }
+}
